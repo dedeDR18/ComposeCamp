2ad571d70d730efbf7136f422507071511c1f6cd
dbesar <dededarirahmadi@gmail.com>
2022-10-23 22:03:02 +0700

    second

--- app/src/main/java/com/example/composecamp/kotlin/HigherOrderFunction.kt
+++ app/src/main/java/com/example/composecamp/kotlin/HigherOrderFunction.kt
@@ -64,13 +64,56 @@ fun main() {
 //    }
 
     //map
-    val fullMenu = cookies.map {
-        "${it.name} - $${it.price}"
+//    val fullMenu = cookies.map {
+//        "${it.name} - $${it.price}"
+//    }
+//
+//    println("Full Menu : ")
+//    fullMenu.forEach {
+//        println(it)
+//    }
+
+    //filter
+//    val softBakedMenu = cookies.filter {
+//        it.softBaked
+//    }
+//
+//    println("Soft cookies:")
+//    softBakedMenu.forEach {
+//        println("${it.name} - $${it.price}")
+//    }
+
+    //groupBy
+//    val groupedMenu = cookies.groupBy { it.softBaked }
+//
+//    val softBakedMenu = groupedMenu[true] ?: listOf()
+//    val crunchyMenu = groupedMenu[false] ?: listOf()
+//
+//    println("Soft cookies:")
+//    softBakedMenu.forEach {
+//        println("${it.name} - $${it.price}")
+//    }
+//    println("Crunchy cookies:")
+//    crunchyMenu.forEach {
+//        println("${it.name} - $${it.price}")
+//    }
+
+    //fold
+//    val totalPrice = cookies.fold(0.0) {total, cookie ->
+//        total + cookie.price
+//    }
+//
+//    println("Total price: $${totalPrice}")
+
+
+    //sortedBy
+    val alphabeticalMenu = cookies.sortedBy {
+        it.name
     }
 
-    println("Full Menu : ")
-    fullMenu.forEach {
-        println(it)
+    println("Alphabetical menu:")
+    alphabeticalMenu.forEach {
+        println(it.name)
     }
 
 }
